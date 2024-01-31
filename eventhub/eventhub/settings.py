7dfd46a673751e26ed3d0218269e3ad2f458a65b
Jhone <jleandro.dev@gmail.com>
2024-01-31 18:15:55 -0300

    feat(database): establish configured database connection

--- eventhub/eventhub/settings.py
+++ eventhub/eventhub/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'eventhub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'eventhub',
+        'USER': 'root',
+        'PASSWORD': 'localhost123',
+        'HOST': 'localhost',
+        'PORT': 3306
     }
 }
 
