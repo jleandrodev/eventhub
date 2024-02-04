2298aea8283fa2dd6806b2cceb8f290eb5c35a1f
Jhone <jleandro.dev@gmail.com>
2024-02-04 11:20:46 -0300

    config: Modify fercel.json

--- eventhub/eventhub/settings.py
+++ eventhub/eventhub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-03l%e-+dlnama!+wxa4jnkd*_*0qvrp-#bc=*6^qw_k%(^^klh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
