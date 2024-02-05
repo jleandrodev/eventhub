c57c62afc78843e8d26794d8bb24295853df8f88
Jhone <jleandro.dev@gmail.com>
2024-02-05 18:08:46 -0300

    config: setting environment variables

--- eventhub/build_files.sh
+++ eventhub/build_files.sh
@@ -1,2 +1,3 @@
 pip install -r requirements.txt 
-python3.9 manage.py collectstatic --clear --noinput --settings=eventhub.settings.production
+python3.9 manage.py collectstatic --clear --noinput --settings=eventhub.settings
+
